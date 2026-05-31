994ca37607947b481fe8e96c5843a838826f27bc
Developer <developer@bill-manager.local>
2026-05-31 23:47:01 +0800

    fix: regenerate Turso auth token

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: false },
   env: {
     DATABASE_URL: 'libsql://bill-manage-xuchi1368.aws-ap-northeast-1.turso.io',
-    TURSO_AUTH_TOKEN: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODAyMzc2MzksImlkIjoiMDE5ZTdlNmUtMTAwMS03ZDhlLTg3YjUtYzJkNTM4NjFmOTFmIiwicmlkIjoiOTAzNWE5YWItZmQ2NC00NjE5LWIyNTYtYzRmZjA5ODFlYmYwIn0.Cgze6THLSGs_iDKg9QqyezQ_P8JUkLA95_xP0IGmcS532Iqbj9UqWvMg2QT0HmX4zpnSDaBhuOsERh1tjOVBCA',
+    TURSO_AUTH_TOKEN: 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODAyNDAwMDIsImlkIjoiMDE5ZTdlNmUtMTAwMS03ZDhlLTg3YjUtYzJkNTM4NjFmOTFmIiwicmlkIjoiOTAzNWE5YWItZmQ2NC00NjE5LWIyNTYtYzRmZjA5ODFlYmYwIn0.nYW3MSzvl8wKPlCYAMN0dSk6Cp0WQYGNCdP1tnEe7XRtoNMWwjmokUvR0j3HiHJX6qilVgMu2Y5ubnLCjIIBCQ',
     JWT_SECRET: 'bill-manager-jwt-secret-2026-change-in-production',
   },
 };
